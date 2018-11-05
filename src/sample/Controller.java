b847edcceddbed59606915103b0feca6e77c1b4d
ViktorVx <victorptrv@yandex.ru>
2018-11-05 16:39:42 +0300

    Add operation-buttons

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -5,16 +5,59 @@ import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.event.ActionEvent;
 
+import java.math.BigDecimal;
+
+
+enum Operation{
+    PLUS, MINUS, DIVISION, MULTIPLY
+}
 
 public class Controller {
 
     @FXML
     private TextField txField;
 
+    BigDecimal num1 = new BigDecimal(0);
+    BigDecimal num2 = new BigDecimal(0);
+    Operation operation;
+
     @FXML
     public void handlePushButton(ActionEvent event){
         Button bt = (Button) event.getSource();
-        txField.setText(txField.getText().concat(bt.getText()));
+        if (txField.getText().equals("0")) {
+            txField.setText(bt.getText());
+        } else {
+            txField.setText(txField.getText().concat(bt.getText()));
+        }
     }
+
+    @FXML
+    public void handleBackSpaceButton() {
+        String s = txField.getText();
+        txField.setText(s.substring(0, s.length() - 1));
+
+    }
+
+    @FXML
+    public void handleClearButton(){
+        txField.setText("0");
+    }
+
+    @FXML
+    public void handlePlusButton() {
+        num1 = new BigDecimal(txField.getText());
+        operation = Operation.PLUS;
+        txField.setText("");
+    }
+
+    @FXML
+    public void handleResultButton() {
+        num2 = new BigDecimal(txField.getText());
+        if (operation.equals(Operation.PLUS)) {
+            txField.setText(num1.add(num2).toString());
+            operation = null;
+        }
+    }
+
 
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -8,11 +8,16 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 
+    //todo insert testing with Mockito
+    //todo ? insert mining ?
+    //todo insert multilingualism
+
+
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 300, 320));
         primaryStage.show();
     }
 
