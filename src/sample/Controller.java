457f9a0557a8796724e7a0a0edf745c9b9fe1fc0
ViktorVx <victorptrv@yandex.ru>
2018-11-05 15:41:02 +0300

    Add digit-buttons handler

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,20 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.event.ActionEvent;
+
+
 public class Controller {
+
+    @FXML
+    private TextField txField;
+
+    @FXML
+    public void handlePushButton(ActionEvent event){
+        Button bt = (Button) event.getSource();
+        txField.setText(txField.getText().concat(bt.getText()));
+    }
+
 }
